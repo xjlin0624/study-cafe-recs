7eb1dcb26ddae7b91d89ce24403b36ebeeee5a2b
xjlin0624 <ciellin0624@gmail.com>
2025-09-22 12:58:20 -0700

    styling show, new, edit pages

--- seeds/index.js
+++ seeds/index.js
@@ -17,13 +17,17 @@ const seedDB = async() => {
     await Cafe.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const cafe = new Cafe({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
-        })
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description this is a placeholder description',
+            price
+        });
         await cafe.save();
-    }
-}
+    };
+};
 
 seedDB().then(() => {
     mongoose.connection.close()
