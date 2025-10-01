c56cf432cfbf121e784602480cb5a441c165f7da
xjlin0624 <ciellin0624@gmail.com>
2025-09-30 23:38:06 -0700

    added wifi prompt for user reviews

--- models/review.js
+++ models/review.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const ReviewSchema = new Schema({
     body: String,
     rating: Number,
+    wifi: String,
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
