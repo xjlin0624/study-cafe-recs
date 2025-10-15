b8a0b835a1b44bde6895663c09bbb6b74488c709
xjlin0624 <ciellin0624@gmail.com>
2025-10-14 23:56:51 -0700

    initialized helmet

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -60,6 +61,7 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({contentSecurityPolicy:false}));
 
 app.use(passport.initialize());
 app.use(passport.session());
